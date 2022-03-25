ff48b7c136a40ab403c26bb5e8042cdeff486e8e
chireh mohamed abdi <chireh.mohamed@outlook.fr>
2022-03-25 21:05:26 +0300

    pratice 1 last modification

--- Practice1.py
+++ Practice1.py
@@ -1,10 +1,8 @@
-from crypt import methods
-from curses import flash
-from flask import Flask, redirect, render_template, send_file, send_from_directory, url_for, request
+import os
+from flask import Flask, redirect, render_template, send_file, send_from_directory, url_for, request, flash
 from flask_pymongo import PyMongo
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
-import os
 
 
 
@@ -12,21 +10,22 @@ import os
 app = Flask(__name__)
 #This is my secret key
 app.config['SECRET_KEY'] = b'X\x98\xb6\xaf5;\x00\xcb\xcf\xba\xbc\xb4\x98/\xc1`'
-app.config['MONGO_URI'] = 'mongodb://localhost:27018/practice1'
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/WAD'
 mongo = PyMongo(app)
-
+UPLOAD_FOLDER = 'upload'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 #Redirect default route to the profile page route
 @app.route("/")
-def index():
-    return render_template('index.html')
-
+def source():
+    return render_template("index.html")
 
 #Display Sign Up file who is in the template folder
 @app.route('/SingUp', methods=['GET','POST'])
 def SignUp():
     #Display sign up page 
     if request.method == "GET":
-        return render_template("SignUp.html")
+        return render_template("SingUp.html")
     else:
         #store username and password from form
         username = request.form.get('username')
@@ -45,14 +44,13 @@ def SignUp():
             
             #flash message for succes
             flash('Signed in !!!!')
-            redirect('/SingIn')
-
+            return redirect('/SingIn')
 #Display Sign In or login page
-@app.route('/SingUp', methods=['GET','POST'])
-def SignUp():
+@app.route('/SingIn', methods=['GET','POST'])
+def SignIn():
     #Display sign up page 
     if request.method == "GET":
-        return render_template("SignIn.html")
+        return render_template("SingIn.html")
     else:
          #store username and password from form
         username = request.form.get('username')
@@ -67,7 +65,32 @@ def SignUp():
             flash('password or username incorrect ')
             return render_template("SignIn.html")
 
-
+@app.route('/secret_page')
+def secret_page():
+    return render_template("secret_page.html")
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+@app.route('/upload',methods=['GET','POST'])
+def upload():
+    if request.method == "GET":
+        return render_template("upload.html")
+    else :
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',filename=filename))
+@app.route('/upload/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],filename)
 #Define the icon of the site
 @app.route("/favicon.ico")
 def favicon():
---
+++ README.md
@@ -0,0 +1,22 @@
+# ITMO WAD (Web Application Development) course. Spring 2022
+
+## First Practice
+
+Task Done !
+
+## 1. Create a flask app which connects to MongoDB and runs at ‘<http://localhost:5000>’
+
+## 2. Authentication
+
+User can sign up at ‘/signup’
+Login page at ‘/auth’ .If success, return a secret page to user.
+Store username, password in database
+
+## 3. Implement image upload feature
+
+Form to upload image at ‘/upload’
+Save uploaded image to folder ‘upload’ on server side, allow only specified extensions
+Redirect to ‘/uploaded/filename’ which shows the uploaded image
+Return user back to ‘upload’ with flash error message if there would be any error
+
+But i can't do the flash message
